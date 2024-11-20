288ad3fe045f348020bc1306a0419fa930ceabc8
Lucia-Susanna <lucia_susanna@hotmail.it>
2024-11-20 15:35:59 +0100

    feat: 03 conta vocali

--- snack-funzioni/03 - conta vocali/script.js
+++ snack-funzioni/03 - conta vocali/script.js
@@ -5,9 +5,18 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function vowel(wordA){
+  let counter = 0;
+  for (let i=0; i<wordA.length; i++){
+    if ((wordA[i] === 'a') || (wordA[i] === 'e') || (wordA[i] === 'i') || (wordA[i] === 'o') || (wordA[i] === 'u')){
+      counter+=1
+    }   
+  }
+  return counter
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(vowel(word))
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
